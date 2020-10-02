c6f1cb6d17159a4c825165a265bb72268b28963d
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2020-10-02 14:52:56 +0200

    x

--- Program.cs
+++ Program.cs
@@ -16,29 +16,26 @@ namespace Bank
             Console.WriteLine(author);
             Console.WriteLine();
 
-            SavingsAccount savingsAccount = new SavingsAccount(1, "Jan", "Kowalski", 72080408887);
-            Account savingsAccount2 = new SavingsAccount(2, "Marek", "Nowak",  72080409998);
-            Account savingsAccount3 = new SavingsAccount(3, "Marek", "Nowak",  72080409998);
-            
 
-
-            Account billingAccount = new BillingAccount(4, savingsAccount.FirstName, savingsAccount.LastName, savingsAccount.Pesel);
+            List<Account> accounts = new List<Account>();
+            accounts.Add(new SavingsAccount(1, "Jan", "Kowalski", 72080408887));
+            accounts.Add(new SavingsAccount(2, "Jan", "Kowalski", 72080408887));
+            accounts.Add(new SavingsAccount(3, "Marek", "Nowak", 72080409998));
+            accounts.Add(new BillingAccount(4, "Marek", "Nowak", 72080409998));
+            accounts.Add(new BillingAccount(5, "Marek", "Nowak", 72080409998));
 
             // Nie przejmując się, która klasa tak naprawdę tym drukowaniem się zajmie.
             // Możemy więc jako typ zmiennej podawać od teraz IPrinter:
             IPrinter printer = new Printer();
             
-            string fullName = savingsAccount.GetFullName();
-            Console.WriteLine("Pierwsze konto w systemie dostał(-a): {0}", fullName);
-            printer.Print(savingsAccount);
-            printer.Print(savingsAccount2);
-            
-            printer.Print(billingAccount);
+
+
+            foreach(Account x in accounts)
+            {
+                printer.Print(x);
+            }
 
             
-            //Console.WriteLine(savingsAccount.GenerateAccountNumber(1));
-
-
 
             Console.ReadKey();
         }
